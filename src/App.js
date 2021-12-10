e6ced462cc83c036c19581b1c1fe0c5442538b7d
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-12-10 21:01:19 +0800

    Update: Test passed

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React from "react";
+
+export default class App extends React.Component {
+
+	state = {
+		peopleInSpace: []
+	}
+  
+	render() {
+	  return (
+		<div>
+		  {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+		</div>
+	  )
+	}
+
+	componentDidMount() {
+		return fetch("http://api.open-notify.org/astros.json")
+		.then(res => {return res.json()})
+		.then(json => {this.setState(json)})
+	}
+}
